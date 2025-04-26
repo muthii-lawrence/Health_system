f50a73ee17b87f1a56646c0d66894ddecb226cf7
muthii-lawrence <123471751+muthii-lawrence@users.noreply.github.com>
2025-04-26 22:36:59 +0300

    Delete server directory
    changes

--- server/supabaseClient.js
+++
@@ -1,9 +0,0 @@
-const { createClient } = require("@supabase/supabase-js");
-
-const SUPABASE_URL = "https://rojuahxsijhbgcagsgzy.supabase.co";
-const SUPABASE_KEY =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJvanVhaHhzaWpoYmdjYWdzZ3p5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU2MTgwNjAsImV4cCI6MjA2MTE5NDA2MH0.bDkPFjdlT38h6Q7yA-jPT-zzoSaV7Px3yp2J4u7NO-g";
-
-const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
-
-module.exports = supabase;
